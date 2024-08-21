df4d8f311c4e349872333545775c29f2b0eb7511
Bilal-018 <bilalimran018@gmail.com>
2024-08-21 14:22:59 +0500

    v2fix0016.3

--- src/services/interceptors.tsx
+++ src/services/interceptors.tsx
@@ -9,7 +9,7 @@ function parseJson<T>(json: string): T {
   return JSON.parse(json) as T;
 }
 
-const jwtInterceoptor: AxiosInstance = axios.create({
+const jwtInterceoptor = axios.create({
   baseURL: API_URL,
   headers: {
     'Content-Type': 'text/json;charset=utf-8',
@@ -17,7 +17,7 @@ const jwtInterceoptor: AxiosInstance = axios.create({
     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
     Authorization: '',
   },
-});
+}) as AxiosInstance;
 
 jwtInterceoptor.interceptors.request.use((config: InternalAxiosRequestConfig) => {
   let tokensData: string | null = sessionStorage.getItem('token');
