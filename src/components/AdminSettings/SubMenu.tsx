5c7d5477943a42da67adfecd940002f7a710e79d
Bilal-018 <bilalimran018@gmail.com>
2024-08-26 17:53:06 +0500

    v2fix0034

--- src/components/AdminSettings/SubMenu.tsx
+++ src/components/AdminSettings/SubMenu.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
 import { Box } from '@mui/material';
 import { SUB_MENU } from '../../pages/AdminSettings';
 import EmployeeInfo from './EmployeeInfo';
 import LeaveManagement from './LeaveManagement';
 
-function SubMenu({ activeMenu }: any) {
+interface AdminSettingsSubMenuProps {
+  activeMenu: string;
+}
+
+function SubMenu({ activeMenu }: AdminSettingsSubMenuProps) {
   switch (activeMenu) {
     case SUB_MENU.employeeInfo:
       return <EmployeeInfo />;
     case SUB_MENU.leaveManagent:
       return <LeaveManagement />;
-   
-    // case SUB_MENU.announcement:
-    //   return <AnnouncementsList />;
-    // case SUB_MENU.asset:
-    //   return <Assets />;
     default:
       return <EmployeeInfo />;
   }
 }
 
-function AdminSettingsSubMenu({ activeMenu }: any) {
+function AdminSettingsSubMenu({ activeMenu }: AdminSettingsSubMenuProps) {
   return (
     <Box>
       <SubMenu activeMenu={activeMenu} />
